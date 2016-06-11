3307d3e3631edc231ec94a5f68a1cba14e207d0d
Andrew <azito122@gmail.com>
2016-06-11 15:48:14 -0400

    Initial commit

--- header.php
+++
@@ -1,5 +0,0 @@
-<header>
-<title>Andrew Zito's Division II</title>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
-<link rel="stylesheet" type="text/css" href="style.css">
-</header>
--- js.js
+++
@@ -1,6 +0,0 @@
-$('#dropdown').click(
-	function() {
-		$('.navbar.n1 li:not(#dropdown)').slideToggle(110)
-		// $('.navbar.fall').slideToggle(110)
-		// $('#dropdown').hide()
-	})
